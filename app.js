ca9826afa1bc9eee5d632f653209df8209eb478d
virascarate <virginiascarate@gmail.com>
2021-09-07 22:52:51 -0300

    Ejercicios clase 7/9

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require ('express');
+const path = require ('path')
+
+const app = express();
+
+//const publicPath = path.resolve (__dirname,'./public')
+//app.use(express.static(publicPath))
+
+app.use(express.static(path.resolve(__dirname, "./public")));
+
+
+app.listen (3030,() => {console.log ("Servidor corriendo http://localhost:3030")
+
+})
+
+app.get ('/', (req,res) => {res.sendFile(path.join(__dirname, '/views/home.html'))})
