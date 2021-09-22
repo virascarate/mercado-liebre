775b67f2a47c07464f213b3e762f81509bb8e240
virascarate <virginiascarate@gmail.com>
2021-09-21 22:05:13 -0300

    actualizo hasta 21/9 sin estilos del form

--- app.js
+++ app.js
@@ -16,3 +16,13 @@ app.listen (3030,() => {console.log ("Servidor corriendo http://localhost:3030")
 })
 
 app.get ('/', (req,res) => {res.sendFile(path.join(__dirname, '/views/home.html'))})
+
+app.get ('/register', (req,res) => {res.sendFile(path.join(__dirname, '/views/register.html'))})
+app.post ('/register', (req,res) => {
+    res.redirect('/')
+})
+
+app.get ('/login', (req,res) => {res.sendFile(path.join(__dirname, '/views/login.html'))})
+app.post ('/login', (req,res) => {
+    res.redirect('/')
+})
