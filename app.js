d69480bb4fe8f35da2aca07d87cf550570cb7c30
virascarate <virginiascarate@gmail.com>
2021-09-21 22:49:41 -0300

    Corrijo indentación y convierto search en form

--- app.js
+++ app.js
@@ -2,27 +2,24 @@
 
 const express = require ('express');
 const path = require ('path')
-
 const app = express();
 
-//const publicPath = path.resolve (__dirname,'./public')
-//app.use(express.static(publicPath))
-
+//RUTA DE LAS IMAGENES
 app.use(express.static(path.resolve(__dirname, "./public")));
 
+//PUERTO DONDE CORRE EL SERVIDOR
+app.listen (3030,() => {console.log ("Servidor corriendo http://localhost:3030")})
 
-app.listen (3030,() => {console.log ("Servidor corriendo http://localhost:3030")
-
-})
-
+//HOME
 app.get ('/', (req,res) => {res.sendFile(path.join(__dirname, '/views/home.html'))})
-
+//REGISTER
+//.get direccion a REGISTER y te para en esa ruta.
 app.get ('/register', (req,res) => {res.sendFile(path.join(__dirname, '/views/register.html'))})
-app.post ('/register', (req,res) => {
-    res.redirect('/')
-})
-
+//.post envia como rta /register
+app.post ('/register', (req,res) => {res.redirect('/')})
+//LOGIN
 app.get ('/login', (req,res) => {res.sendFile(path.join(__dirname, '/views/login.html'))})
-app.post ('/login', (req,res) => {
-    res.redirect('/')
-})
+app.post ('/login', (req,res) => {res.redirect('/')})
+
+app.get ('/search', (req,res) => {res.sendFile(path.join(__dirname, '/views/search.html'))})
+app.post ('/search', (req,res) => {res.redirect('/')})
